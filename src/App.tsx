eb588dded9c911abfa4a57aad16c4ebadee620f7
vijay.krishna <vijay.krishna@quantilope.com>
2021-11-27 23:06:38 +0100

    react valtion theme picker

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,31 @@
 import React from "react";
 import "./App.css";
-
-interface Todo {
-  name: string;
-  isDone: boolean;
-}
-
+import { ThemedElements } from "./ThemedElements";
+import { changeTheme, useTheme } from "./theme.store";
 function App() {
-  const [todos, setTodos] = React.useState<Todo[]>([
-    {
-      name: "Learn Solid",
-      isDone: false,
-    },
-  ]);
-
-  const todoRef = React.useRef<HTMLInputElement>(null);
-
-  const onToggleTodo = (index: number) => () => {
-    const newTodos = [...todos];
-    newTodos[index].isDone = !newTodos[index].isDone;
-    setTodos(newTodos);
-  };
-
-  const onAddTodo = () => {
-    if (todoRef.current) {
-      const name = todoRef.current.value;
-      if (name) {
-        setTodos([...todos, { name, isDone: false }]);
-        todoRef.current.value = "";
-      }
-    }
-  };
-
+  const themeStore = useTheme();
+  const theme = themeStore[themeStore.mode];
   return (
-    <div className="App">
-      <div className="md">
-        <h2 className="text-lg font-bold">Create your todos here</h2>
-        <div className="flex space-x-4 py-10 px-28">
-          <input
-            ref={todoRef}
-            className="w-1/2 h-8 p-4 border-4 border-indigo-400 border-opacity-100"
-            type="text"
-            placeholder="Enter a Todo"
-            onKeyPress={(e) => {
-              e.which === 13 && onAddTodo();
-            }}
-          />
-          <button
-            onClick={onAddTodo}
-            className="w-28 h-10 border-4 bg-indigo-400 rounded-sm border-indigo-400"
+    <div className={`App h-screen	${theme.colors.bodyBg}`}>
+      <div className="2xl:container md:container sm:container lg:container mx-auto px-10">
+        <div className="flex space-x-5">
+          <h2
+            className={`text-3xl my-5 ${theme.colors.text400} uppercase font-bold`}
           >
-            Add
-          </button>
+            React+Valtio
+          </h2>
+          <i
+            onClick={() =>
+              changeTheme(themeStore.mode === "light" ? "dark" : "light")
+            }
+            className={`${theme.colors.themePicker} ${
+              themeStore.mode === "light"
+                ? `theme-picker-dark `
+                : "theme-picker-light"
+            }`}
+          />
         </div>
-        <div className="flex space-y-8 flex-col">
-          {todos.map((todo, index) => (
-            <div
-              key={index}
-              className="flex items-center justify-items-center flex-row space-x-4 px-28"
-            >
-              <input
-                type={"checkbox"}
-                className="w-4 h-4 border-2 border-indigo-400"
-                checked={todo.isDone}
-                onChange={onToggleTodo(index)}
-              />
-              <span className={`${todo.isDone ? "line-through" : ""} text-lg`}>
-                {todo.name}
-              </span>
-            </div>
-          ))}
-        </div>
+        <ThemedElements />
       </div>
     </div>
   );
---
+++ src/theme.store.ts
@@ -0,0 +1,69 @@
+import { proxy, useSnapshot } from "valtio";
+
+interface Colors {
+  bg400: string;
+  border400: string;
+  text400: string;
+  placeholder400: string;
+  themePicker: string;
+  bodyBg: string;
+  hoverText: string;
+  hoverBg: string;
+  outline400: string;
+}
+
+interface Theme {
+  light: {
+    colors: Colors;
+  };
+  dark: {
+    colors: Colors;
+  };
+  mode: "light" | "dark";
+}
+
+export const themeStore = proxy<Theme>({
+  light: {
+    colors: {
+      bg400: "bg-blue-400",
+      border400: "border-blue-400",
+      outline400: "outline-blue-400",
+      text400: "text-black",
+      placeholder400: "placeholder-gray-300",
+      themePicker: "bg-gray-700",
+      bodyBg: "bg-gray-50",
+      hoverText: "hover:text-blue-200",
+      hoverBg: "hover:bg-blue-300",
+    },
+  },
+  dark: {
+    colors: {
+      bg400: "bg-gray-700",
+      border400: "border-blue-400",
+      outline400: "outline400-indigo-400",
+      text400: "text-gray-100",
+      placeholder400: "placeholder-gray-300",
+      themePicker: "bg-gray-100",
+      bodyBg: "bg-gray-700",
+      hoverText: "hover:text-blue-400",
+      hoverBg: "hover:bg-blue-300",
+    },
+  },
+  mode: "light",
+});
+
+export const useTheme = () => useSnapshot(themeStore);
+
+export const changeTheme = (mode: "light" | "dark") => {
+  themeStore.mode = mode;
+};
+
+export const toRed = () => {
+  themeStore[themeStore.mode].colors.bg400 = "bg-red-200";
+  themeStore[themeStore.mode].colors.text400 = "text-red-400";
+};
+
+export const toYellow = () => {
+  themeStore[themeStore.mode].colors.bg400 = "bg-yellow-200";
+  themeStore[themeStore.mode].colors.text400 = "text-yellow-400";
+};
